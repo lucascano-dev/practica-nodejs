62b31a2b88c97e1ef12f4af4e60d6e5321868d9a
Lucas Aaron Cano <lucas_cano@outlook.com>
2023-12-03 03:47:08 -0300

    uso de metodos de solicitudes HTTP (GET, PUT, PATCH, DELETE)

--- routers/programacion.js
+++ routers/programacion.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
-const {infoCursos} = require('../datos/cursos.js');
+const { infoCursos } = require('../datos/cursos.js');
 
 const routerProgramacion = express.Router();
+
+//Middleware: Las funciones middleware se ejecutan (1) despues de recibir una solicitud. (2) antes de enviar una respuesta
+routerProgramacion.use(express.json());
+
+
 
 // CURSOS DE PROGRAMACION
 routerProgramacion.get("/", (req, res) => {
@@ -31,9 +36,57 @@ routerProgramacion.get("/:lenguaje", (req, res) => {
 
     if (resultados.length === 0) {
       return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`);
+      // return res.status(404).end()
     }
     res.send(JSON.stringify(resultados));
   });
 });
+
+
+routerProgramacion.post('/', (req, res) => {
+  let cursoNuevo = req.body;
+  infoCursos.programacion.push(cursoNuevo);
+  res.send(JSON.stringify(infoCursos.programacion));
+  console.log('Entra POST')
+});
+
+routerProgramacion.put('/:id', (req, res) => {
+  const cursoActualizado = req.body;
+  const id = req.params.id;
+
+  const indice = infoCursos.programacion.findIndex(curso => curso.id == id);
+
+  if (indice >= 0) {
+    infoCursos.programacion[indice] = cursoActualizado; 
+  }
+  res.send(JSON.stringify(infoCursos.programacion));
+
+});
+
+routerProgramacion.patch('/:id', (req, res) => {
+  const infoActualizada = req.body;
+  const id = req.params.id;
+
+  const indice = infoCursos.programacion.findIndex(curso => curso.id == id);
+
+  if (indice >= 0 ) {
+    const cursoAModificar = infoCursos.programacion[indice];
+    Object.assign(cursoAModificar, infoActualizada);
+  }
+  res.send(JSON.stringify(infoCursos.programacion));
+});
+
+routerProgramacion.delete('/:id', (req, res) =>{
+  const id = req.params.id;
+
+  const indice = infoCursos.programacion.findIndex(curso => curso.id == id);
+
+  if (indice >= 0) {
+    infoCursos.programacion.splice(indice, 1);
+  }
+  res.send(JSON.stringify(infoCursos.programacion));
+})
+
+
 
 module.exports = routerProgramacion;
